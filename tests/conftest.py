aac2c36407c2847c0555fb54a26d8e8a2a52b866
Phillip Henry <PhillHenry@gmail.com>
2021-04-30 15:52:27 +0100

    Sharing contexts between tests.

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+import logging
+
+import pytest
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+from pyspark.conf import SparkConf
+
+
+def quiet_py4j():
+    """Suppress spark logging for the test context."""
+    logger = logging.getLogger('py4j')
+    logger.setLevel(logging.WARN)
+
+
+@pytest.fixture(scope="session")
+def spark_session(request):
+    """Fixture for creating a spark context."""
+
+    spark = (SparkSession
+             .builder
+             .master('local[2]')
+             .config('spark.jars.packages', 'com.databricks:spark-avro_2.11:3.0.1')
+             .appName('pytest-pyspark-local-testing')
+             .enableHiveSupport()
+             .getOrCreate())
+    request.addfinalizer(lambda: spark.stop())
+
+    quiet_py4j()
+    return spark
+
+
+@pytest.fixture(scope="session")
+def spark_context(request):
+    """ fixture for creating a spark context
+    Args:
+        request: pytest.FixtureRequest object
+    """
+    conf = (SparkConf().setMaster("local[2]").setAppName("pytest-pyspark-local-testing"))
+    sc = SparkContext(conf=conf)
+    request.addfinalizer(lambda: sc.stop())
+
+    quiet_py4j()
+    return sc
+
